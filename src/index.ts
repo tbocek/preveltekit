5ac8fc049afc8393083ae2f304ab74fbc04139d9
Thomas Bocek <tom@tomp2p.net>
2025-10-15 20:16:59 +0200

    release: v1.2.0

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { navigate, route } from './router.js';
+export { navigate, link } from './router.js';
 export type { Routes, Component } from './types.js';
 export { default as Router } from './Router.svelte';
