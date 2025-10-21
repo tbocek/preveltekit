8264bf70f2862e74a228c565465c00f2c241c923
Thomas Bocek <tom@tomp2p.net>
2025-10-21 20:18:14 +0200

    export missing function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { navigate, link } from './router.js';
+export { navigate, link, setupStaticRoutes } from './router.js';
 export type { Routes, Component } from './types.js';
 export { default as Router } from './Router.svelte';
