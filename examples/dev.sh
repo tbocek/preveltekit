a13962f49e8d9b5aa8853f5e6b55a69c911dcbf8
Thomas Bocek <tom@tomp2p.net>
2026-02-10 17:52:09 +0100

    fix entr

--- examples/dev.sh
+++ examples/dev.sh
@@ -22,7 +22,7 @@ echo "Watching for changes..."
 
 # Watch .go files and rebuild on change
 while true; do
-    find . -name '*.go' -not -path '*/dist/*' | entr -d -s '
+    find . -name '*.go' -not -path '*/dist/*' | entr -dn -s '
         echo "Rebuilding..."
         ./build.sh . && curl -s -X POST http://localhost:3001/trigger > /dev/null && echo "Reloaded."
     '
